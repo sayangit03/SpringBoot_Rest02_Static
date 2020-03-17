9a95913e0ef9e522b605b45156d0f67d71c9da30
sayangit03 <sayman.eye@gmail.com>
2020-03-17 11:05:09 +0530

    ok done

--- src/main/java/com/boot/rest/StudentResource.java
+++ src/main/java/com/boot/rest/StudentResource.java
@@ -16,7 +16,7 @@ public class StudentResource {
 
 	@GetMapping("student")
 	public List<Student> getStudent() {
-		System.out.println("ok.. Delivering...");
+		System.out.println("ok.. Delivering.....");
 		List<Student> sList = (List<Student>) sRepo.findAll();
 		return sList;
 	}
