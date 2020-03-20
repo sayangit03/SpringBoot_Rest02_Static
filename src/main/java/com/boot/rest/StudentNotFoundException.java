912281297bc22aded18d29a0a2d8b88ece493ed5
sayangit03 <sayman.eye@gmail.com>
2020-03-21 00:46:32 +0530

    Restful web service with exception

---
+++ src/main/java/com/boot/rest/StudentNotFoundException.java
@@ -0,0 +1,20 @@
+package com.boot.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class StudentNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public StudentNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
--- src/main/java/com/boot/rest/StudentResource.java
+++ src/main/java/com/boot/rest/StudentResource.java
@@ -1,12 +1,18 @@
 package com.boot.rest;
 
+import java.net.URI;
+import java.rmi.StubNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class StudentResource {
@@ -21,12 +27,27 @@ public class StudentResource {
 		List<Student> sList = (List<Student>) sRepo.findAll();
 		return sList;
 	}
+	
+	@GetMapping("student/{id}")
+	public Student getStudent(@PathVariable int id) {
+		System.out.println("ok.. Delivering.....");
+		System.out.println("adding logs...");
+		Optional<Student> sList = sRepo.findById(id);
+		if(!sList.isPresent()) {
+			throw new StudentNotFoundException("Student not found!");
+		}
+		
+		return sList.get();
+	}
 
 	@PostMapping("student")
-	public Student createStudent(@RequestBody Student s) {
+	public ResponseEntity<Object> createStudent(@RequestBody Student s) {
 		System.out.println("Creating student... : "+s.getsId());
 		sRepo.save(s);
-		return s;
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(s.getsId()).toUri();
+		System.out.println(location);
+		return ResponseEntity.created(location).build();
 	}
 
 }
