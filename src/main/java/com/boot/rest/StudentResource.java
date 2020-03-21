4472e76106186c84d8217bcf7c0b815765281d8c
sayangit03 <sayman.eye@gmail.com>
2020-03-21 21:29:44 +0530

    Internationalization i18n

--- src/main/java/com/boot/rest/StudentResource.java
+++ src/main/java/com/boot/rest/StudentResource.java
@@ -2,17 +2,21 @@ package com.boot.rest;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.hateoas.*;
@@ -24,6 +28,14 @@ public class StudentResource {
 	@Autowired
 	StudentRepository sRepo;
 	
+	@Autowired
+	MessageSource messageSource;
+	
+	@GetMapping("hello")
+	public String sayHello() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+
 	@GetMapping("student")
 	public List<Student> getStudent() {
 		System.out.println("ok.. Delivering.....");
